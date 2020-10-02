2d526ee8310549d4a56e16d66125e3cec0ddb600
UncleKhab <cobrosoffice@gmail.com>
2020-10-02 19:38:48 +0300

    Project Functionality Finished

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,44 +6,88 @@ function d(selector){
 };
 
 // 2. constructor function for email list
-function item_constructor(tag, clas, cont){
+function item_constructor(tag, clas, cont, label){
   let element = document.createElement(tag);
       element.className = clas;
-      element.innerHTML = cont;
+      if(label === undefined){
+        element.innerHTML = cont;
+      } else {
+        element.innerHTML = `${label}: ${cont}`;
+      }
+      
   
   return element;
 }
+// 3. email display function
 
-// 3. function for event listener for subject lines
+function email_display(content, status){
+  let sender      = item_constructor('p', 'email_sender', content.sender, 'From');
+  
+  let recipient_list = item_constructor('p', 'recipients', '', 'To');
+  let recipients = content.recipients;
+      recipients.forEach(recipient => {
+        let temp = item_constructor('em', 'recipient', recipient);
+        recipient_list.appendChild(temp)
+      })
 
-function em_r(id, status){
-  load_email(id, status);
+  let subject     = item_constructor('h2', 'email_subject', content.subject, 'Subject');
+  let time        = item_constructor('small', 'email_timestamp', content.timestamp, 'Date Sent');
+  let body        = item_constructor('p', 'email_body', content.body);
+  
+  let email = document.createElement('div');
+      email.className = 'email-view-container';
+      if(status === 'r' && content.archived === true){
+        let unarchive = item_constructor('button', 'btn-small', 'Unarchive');
+        unarchive.setAttribute('onclick', `archive_status(${content.id}, 't')`);
+        email.appendChild(unarchive);
+      }
+
+      email.appendChild(sender);
+      email.appendChild(recipient_list);
+      email.appendChild(subject);
+      email.appendChild(time);
+      email.appendChild(body);
+  d('#emails-view').innerHTML = '';
+  d('#emails-view').append(email);
 }
 // 4. iterator function for received emails
 
 function add_email_received(content) {
+  // Getting relevant data from each email
   let archived = content.archived;
   let id = content.id;
-  let subject = item_constructor('a', 'email-subject', content.subject);
-      subject.setAttribute('onclick', `em_r(${id}, 'r')`);
+  
+  let subject = item_constructor('a', 'email-subject', content.subject, 'Subject');
+      subject.setAttribute('onclick', `load_email(${id}, 'r')`);
       subject.setAttribute('href', '#');
-  let sender  = item_constructor('p', 'email-sender', content.sender);
-  let time    = item_constructor('small', 'email-time', content.timestamp);
+  
+  let sender  = item_constructor('p', 'email-sender', content.sender, 'From');
+  let time    = item_constructor('small', 'email-time', content.timestamp, 'Date Sent');
+  
   let archive = item_constructor('button', 'btn-small', 'Archive');
   if(archived === false){
-    archive.innerHTML = 'Archive' 
-    archive.setAttribute('onclick', `archive_status(${id}, 't')`)
+    archive.innerHTML = 'Archive'; 
+    archive.setAttribute('onclick', `archive_status(${id}, 'f')`);
   }else{
-    archive.innerHTML = 'Unarchive'
-    archive.setAttribute('onclick', `archive_status(${id}, 'f')`)
+    archive.innerHTML = 'Unarchive';
+    archive.setAttribute('onclick', `archive_status(${id}, 't')`);
   }
-  
+
+  let reply = item_constructor('button', 'btn-small', 'Reply');
+      reply.setAttribute('onclick', `reply_email(${id})`);
+
   let email = document.createElement('div');
-      email.className = 'email-container';
+      if(content.read === true){
+        email.className += 'b-gray ';
+      } else {
+        email.className += 'b-white ';
+      }
+      email.className += 'email-container';
       email.appendChild(subject);
       email.appendChild(sender);
       email.appendChild(time);
       email.appendChild(archive);
+      email.appendChild(reply);
   
 
   d('#emails-view').append(email);
@@ -55,34 +99,42 @@ function add_email_received(content) {
 // 5. iterator function for sent emails
 
 function add_email_sent(content) {
-  let subject     = item_constructor('a', 'email-subject', content.subject);
-      subject.setAttribute('onclick', `em_r(${content.id}, 's')`)
+  let subject     = item_constructor('a', 'email-subject', content.subject, 'Subject');
+      subject.setAttribute('onclick', `load_email(${content.id}, 's')`);
       subject.setAttribute('href', '#');
-  let recipients  = item_constructor('p', 'email-recipient', content.recipient);
-  let time        = item_constructor('small', 'email-sender', content.timestamp);
+  
+  let recipient_list = item_constructor('p', 'recipients', '', 'To');
+  let recipients  = content.recipients;
+      
+      recipients.forEach(recipient => {
+        let temp  = item_constructor('em', 'recipient', recipient);
+        recipient_list.appendChild(temp);
+      })
+  
+  let time  = item_constructor('small', 'email-sender', content.timestamp, 'Date Sent');
       
   let email = document.createElement('div');
       email.className = 'email-container';
       email.appendChild(subject);
-      email.appendChild(recipients);
+      email.appendChild(recipient_list);
       email.appendChild(time);
-
+      
   d('#emails-view').append(email);
 
-  console.log(content)
 }
 
 // 6. Changed Archived Status
 
 function archive_status(id, status){
-  if(status === 't'){
+  // if status comes 't' 
+  if(status === 'f'){
     fetch(`/emails/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
         archived: true
       })
     })
-    console.log("archived")
+    load_mailbox('inbox'); 
   } else {
     fetch(`/emails/${id}`, {
       method: 'PUT',
@@ -90,12 +142,42 @@ function archive_status(id, status){
         archived: false
       })
     })
-    console.log("unarchived")
+    load_mailbox('inbox');
   }
-
+  
 }
 
+// 7 Send Email Function
 
+function send_email(){
+  d('#compose-form').onsubmit = function() {
+    let recipients = d('#compose-recipients').value;
+    let subject = d('#compose-subject').value;
+    let body = d('#compose-body').value;
+    
+    fetch('/emails', {
+        method: 'POST',
+        body: JSON.stringify({
+            recipients: recipients,
+            subject: subject,
+            body: body
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.message === 'Email sent successfully.'){
+            load_mailbox('sent');
+            d('#alert-show').innerHTML = result.message;
+        } else {
+            d('#alert-show').innerHTML = result.error;
+            return false;
+        }
+    });
+    return false;
+  };
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------------DOM Loaders
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
@@ -106,47 +188,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
-
+  
 });
+
+
+function reply_email(id){
+  d('#emails-view').innerHTML = '';
+  d('#emails-view').style.display = 'none';
+  d('#compose-view').style.display = 'block';
+
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+    // Changing the read status when email is accessed
+    d('#compose-recipients').value = email.sender;
+    d('#compose-subject').value = `RE: ${email.subject}`
+    d('#compose-body').value = `On ${email.timestamp} , ${email.sender} wrote : ${email.body}`
+  })
+  
+  send_email();
+}
+
 
 function compose_email() {
 
   // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
   d('#emails-view').innerHTML = '';
+  document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
-
-  d('#compose-form').onsubmit = function() {
-      let recipients = d('#compose-recipients').value;
-      let subject = d('#compose-subject').value;
-      let body = d('#compose-body').value;
-      
-      fetch('/emails', {
-          method: 'POST',
-          body: JSON.stringify({
-              recipients: recipients,
-              subject: subject,
-              body: body
-          })
-      })
-      .then(response => response.json())
-      .then(result => {
-          if (result.message === 'Email sent successfully.'){
-              load_mailbox('sent');
-              d('#alert-show').innerHTML = result.message;
-          } else {
-              d('#alert-show').innerHTML = result.error;
-              return false;
-          }
-      });
-      return false;
-    };
+ 
+  send_email();
 }
+
+
 
 function load_mailbox(mailbox) {
   
@@ -170,15 +249,16 @@ function load_mailbox(mailbox) {
   
 }
 
+
+
 function load_email(id, status) {
-  d("#emails-view").style.display = 'none';
+  d("#emails-view").style.display = 'block';
   d('#compose-view').style.display = 'none';
 
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
     // Changing the read status when email is accessed
-
     if((email.read === false) && (status === 'r')){
       fetch(`/emails/${id}`, {
         method: 'PUT',
@@ -187,8 +267,7 @@ function load_email(id, status) {
         })
       })
     }
+    email_display(email, status);
     
-  })
-
-  
+  }) 
 }
